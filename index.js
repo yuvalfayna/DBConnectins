f17f344130f47c0e8963f37775423d08243a3293
yuvalfayna <yovalfayna98@gmail.com>
2024-10-04 01:02:08 +0300

    v3connect

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-import redisServer from './redis_connection.js'; 
 import mongoServer from './mongo_connection.js';
+import redisServer from './redis_connection.js';
+
+const startServices = async () => {
+    try {
+        await mongoServer;  
+        console.log("MongoDB service started successfully.");
+
+        await redisServer;  
+        console.log("Redis service started successfully.");
+
+    } catch (error) {
+        console.error("Error starting services:", error);
+    }
+};
+
+startServices();
