dd574ea1ff8d1bd7e88b6605b7c043ce2d8f8a02
yuvalfayna <yovalfayna98@gmail.com>
2024-10-04 00:58:28 +0300

    v2connect

--- mongo_connection.js
+++ mongo_connection.js
@@ -43,8 +43,8 @@ app.get('/array', async (req, res) => {
   }
 });
 
-// Use the PORT environment variable or default to port 3000
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
